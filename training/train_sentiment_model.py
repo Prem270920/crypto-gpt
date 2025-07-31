cd5923449369ff2e0181fdf232cc326d7be025a7
Prem270920 <sathishpremkumar27@gmail.com>
2025-07-31 22:01:12 +1000

    Update: fine-tune the model using GridSearchCV withe conbo of LogisticRegression and LinearSVC

--- training/train_sentiment_model.py
+++ training/train_sentiment_model.py
@@ -1,7 +1,8 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
+from sklearn.svm import LinearSVC
 from sklearn.pipeline import Pipeline
 from sklearn.metrics import classification_report, accuracy_score
 import joblib
@@ -12,18 +13,25 @@ df.dropna(subset=['headline'], inplace=True)
 
 X_train, X_test, y_train, y_test = train_test_split(df['headline'], df['sentiment'], test_size=0.2, random_state=42)
 
-# This pipeline bundles the two steps: vectorizing text and classifying it.
-text_clf_pipeline = Pipeline([('tfidf', TfidfVectorizer()),('clf', LogisticRegression(max_iter=1000))])
+pipeline = Pipeline([('tfidf', TfidfVectorizer()), ('clf', LogisticRegression()),])
 
-text_clf_pipeline.fit(X_train, y_train) 
+#parameters for GridSearchCV to test with single words and word pairs
+parameters = {'tfidf__ngram_range': [(1, 1), (1, 2)], 'clf': [LogisticRegression(max_iter=1000), LinearSVC(max_iter=1000)]}
 
-predictions = text_clf_pipeline.predict(X_test)
+#Performing the Grid Search
+grid_search = GridSearchCV(pipeline, parameters, n_jobs=-1, cv=5, verbose=1)
+
+grid_search.fit(X_train, y_train)
+
+#Evaluate the best Model
+best_model = grid_search.best_estimator_
+predictions = best_model.predict(X_test)
 
 print("\nClassification Report:")
 print(classification_report(y_test, predictions))
 
 print(f"Overall Accuracy: {accuracy_score(y_test, predictions):.2%}")
 
-joblib.dump(text_clf_pipeline, "models/traditional_sentiment_model.pkl")
+joblib.dump(best_model, "../models/traditional_sentiment_model.pkl")
 
 
