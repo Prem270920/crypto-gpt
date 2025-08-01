d3c5590c56ba2d9acae5691e2058aab7dac562b5
Prem270920 <sathishpremkumar27@gmail.com>
2025-08-02 00:55:01 +1000

    modified the training model

--- training/train_sentiment_model.py
+++ training/train_sentiment_model.py
@@ -29,8 +29,6 @@ df.dropna(subset=['headline'], inplace=True)
 
 df['cleaned_headline'] = df['headline'].apply(clean_text)
 
-
-
 X_train, X_test, y_train, y_test = train_test_split(df['cleaned_headline'], df['sentiment'], test_size=0.2, random_state=42)
 
 pipeline = Pipeline([('tfidf', TfidfVectorizer()), ('clf', LogisticRegression()),])
@@ -53,5 +51,3 @@ print(classification_report(y_test, predictions))
 print(f"Overall Accuracy: {accuracy_score(y_test, predictions):.2%}")
 
 joblib.dump(best_model, "models/traditional_sentiment_model.pkl")
-
-
